ab09e1eb01cac035cbd369fb5956db01cb714069
chetan shinde <chetanshinde3726@gmail.com>
2026-04-27 18:54:56 +0530

    Delete 03-Java Module/Assignments/Assignment-01 directory

--- 03-Java Module/Assignments/Assignment-01/Task-1/String_to_Int_Convert.java
+++
@@ -1,8 +0,0 @@
-class String_to_Int_Convert{
-public static void main(String[] args){
-System.out.println("Enterd input-1: "+args[0]);
-System.out.println("Enterd input-2: "+args[1]);
-int a=Integer.parsenInt(args[0]);
-int b=Integer.parsenInt(args[1]);
-}
-}
